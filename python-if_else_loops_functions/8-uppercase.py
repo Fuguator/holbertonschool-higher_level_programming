58b5f9c5611a59682856eb513b5c01ebf50ddf21
Your Name <you@example.com>
2024-09-03 17:24:03 +0400

    added

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) in range(65, 91):
-        return True
-    else:
-        return False
+def uppercase(str):
+    result = ''
+    for let in str:
+        asci_let = ord(let)
+        if (asci_let >= 97 and asci_let <= 122):
+            up_let = chr(asci_let - 32)
+            result += up_let
+        else:
+            result += chr(asci_let)
+    print("{}".format(result))
