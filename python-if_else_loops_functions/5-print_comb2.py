4908ef22e5d864de06177e448fdec551a6dd164b
Your Name <you@example.com>
2024-09-03 16:30:46 +0400

    fixed

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if x == 0:
-        print("0" + f"{x}", end = "")
-    elif x < 10:
-        print(", " + "0" + f"{x}", end = "")
+    if x < 10:
+        print("0" + f"{x}" + ", ", end = "")
     else:
         print(", " + f"{x}", end = "")
