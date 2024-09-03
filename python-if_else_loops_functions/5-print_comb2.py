07778fa1cc59e1d2d11c336c82f392364ae808b7
Your Name <you@example.com>
2024-09-03 16:42:13 +0400

    fixed

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-numbers = []
-for x in range(100):
-    if x < 10:
-        num_str = "0" + "{}".format(x)
-    else:
-        num_str = "{}".format(x)
-    numbers.append(num_str)
-
-print(", ".join(numbers))
+for num in range(0, 99):
+    print("{:02d}".format(num), end=", ")
+print("99")
