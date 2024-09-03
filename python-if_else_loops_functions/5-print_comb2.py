4af124c9a0f6980911c854f4582670c2ae353922
Your Name <you@example.com>
2024-09-03 16:36:28 +0400

    fixed

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,5 @@ for x in range(100):
     if x < 10:
         print("0" + "{}".format(x), end=", ")
     else:
-        print("{}".format(x), end=", " if x < 99 else "")
+        print("{}".format(x), end=", " if x <= 99 else "")
 
