51a3bf4a37f49668317fd2094c8982385c5ae64f
Your Name <you@example.com>
2024-09-04 15:14:05 +0400

    fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-count = len(sys.argv)
-print(f"{count} arguments:")
-for x in range(count):
-    print(f"{x + 1}: {sys.argv[x + 1]}")
+if __name__ == '__main__':
+    import sys
+    count = len(sys.argv)
+    print(f"{count} arguments:")
+    for x in range(count):
+        print(f"{x + 1}: {sys.argv[x + 1]}")
