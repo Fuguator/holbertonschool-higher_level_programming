a4f9c20828ab002ad32fa6f2561ad6f464070d5e
Your Name <you@example.com>
2024-09-04 15:24:57 +0400

    fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,9 @@
 if __name__ == '__main__':
     import sys
     count = len(sys.argv)
-    if count == 2:
+    if count <= 1:
+        print("0 arguments.")
+    elif count == 2:
         print(f"{count - 1} argument:")
     else:
         print(f"{count - 1} arguments:")
