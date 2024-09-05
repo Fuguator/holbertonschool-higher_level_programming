3099d217ea508cc44fb7668e93210ffa2e611d2f
Your Name <you@example.com>
2024-09-05 14:25:33 +0400

    fixed

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-    
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
@@ -10,4 +10,4 @@ def magic_calculation(a, b):
         return (c)
 
     else:
-        return(sub(a, b))
+        return (sub(a, b))
