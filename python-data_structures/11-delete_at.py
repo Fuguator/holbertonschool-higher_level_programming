9cd8f090d8605828cc413e316b53482130ea9c79
Abbasov Fuad <you@example.com>
2024-09-11 15:43:30 +0400

    Fix_1

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx not in range(my_list):
+    if idx not in range(len(my_list) - 1):
         return my_list
     new_list = []
     for i in range(len(my_list)):
