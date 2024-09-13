1c01efd4c782c68590e40a03fce2f93831a028d7
Abbasov Fuad <you@example.com>
2024-09-13 18:21:24 +0400

    Fix_4: Pycode style

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
-    values, sum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}, 0
+    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum = 0
     for i in range(len(roman_string)):
         if i > 0 and values[roman_string[i]] > values[roman_string[i - 1]]:
             sum += values[roman_string[i]] - (values[roman_string[i - 1]] * 2)
