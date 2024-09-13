0fcc2cab58c9f1bb568d2e72e9c012a97d60bf61
Abbasov Fuad <you@example.com>
2024-09-13 18:14:08 +0400

    Add: 12-main.py 12-roman_to_int.py

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return None
+    values, sum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}, 0
+    for i in range(len(roman_string)):
+        if i > 0 and values[roman_string[i]] > values[roman_string[i - 1]]:
+            sum += values[roman_string[i]] - (values[roman_string[i - 1]] * 2)
+        else:
+            sum += values[roman_string[i]]
+    return sum
