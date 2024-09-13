e9bf4aa9ed9ce5845b529a4ad8958e406bee14d4
Abbasov Fuad <you@example.com>
2024-09-13 18:20:26 +0400

    Fix_3: fixed return value

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
-        return None
+        return 0
     values, sum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}, 0
     for i in range(len(roman_string)):
         if i > 0 and values[roman_string[i]] > values[roman_string[i - 1]]:
