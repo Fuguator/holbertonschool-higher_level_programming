b23c327025e65bc645c16bf85ff330094ad12115
Abbasov Fuad <you@example.com>
2024-09-15 17:35:41 +0400

    Fix_1: comment added

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """returns a sum"""
 def add_integer(a, b=98):
+    """takes a sum of a and b"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
