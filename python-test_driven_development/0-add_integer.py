a4d5752abd5efc3a995aa71a39d46e3065e9dd91
Abbasov Fuad <you@example.com>
2024-09-15 19:11:52 +0400

    .

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """returns a sum"""
+
 def add_integer(a, b=98):
+
     """takes a sum of a and b"""
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
