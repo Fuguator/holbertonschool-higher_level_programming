6bcf74a1005ce70b7e9d226623b32e274198038b
Fuguator <abbsvfuad@gmail.com>
2024-09-18 22:06:05 +0400

    Add: 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Declare class Square"""
+
+
+class Square:
+    """Attributes of class Square"""
+    __size = None
+
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+    def area(self):
+        return self.__size ** 2
