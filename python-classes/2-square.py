7cbf21d0b1e74ea66f910ae577bdfc9928323744
Fuguator <abbsvfuad@gmail.com>
2024-09-18 22:03:00 +0400

    Add: 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Declare class Square"""
+
+
+class Square:
+    """Attributes of class Square"""
+    __size = None
+
+    def __init__(self, size):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
