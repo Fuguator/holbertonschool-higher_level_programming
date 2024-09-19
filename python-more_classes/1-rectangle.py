b01e693116a63e484c728c4c9bb17c59bceb1b79
Fuguator <abbsvfuad@gmail.com>
2024-09-19 19:50:44 +0400

    Fix_1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,15 +7,15 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         return self.__width
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -23,7 +23,7 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+ 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
