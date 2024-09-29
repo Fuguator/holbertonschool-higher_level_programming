985948c5a66b9882c3e064954d69bd4ea0364af0
Fuguator <abbsvfuad@gmail.com>
2024-09-29 18:02:27 +0400

    Fix_1

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -4,6 +4,7 @@
 
 def func(obj, name, value):
     """Add attribute"""
+
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     return setattr(obj, name, value)
