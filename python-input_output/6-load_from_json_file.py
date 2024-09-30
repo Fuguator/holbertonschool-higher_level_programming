631d02c7ddab9fa9306d1d7f2cbe8fa5c88455d7
Fuguator <abbsvfuad@gmail.com>
2024-09-30 16:28:14 +0400

    Add: 6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""load_from_json_file"""
+import json
+
+
+def load_from_json_file(filename):
+    """load_from_json_file"""
+    with open(filename, 'r') as file:
+        return json.loads(file.read())
