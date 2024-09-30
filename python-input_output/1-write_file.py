a9e18ab7b9d56e682fc20f11ed55e19ea8e6916f
Fuguator <abbsvfuad@gmail.com>
2024-09-30 16:06:48 +0400

    Fix_1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """write_file"""
     with open(filename, "a+", encoding='utf-8') as file:
-        return (file.write(text)
+        return file.write(text)
