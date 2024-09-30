d027cc9c698937dedb425c5ba1d43c0dc2faefd0
Fuguator <abbsvfuad@gmail.com>
2024-09-30 16:48:33 +0400

    Fix_2

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,9 +2,9 @@
 """adds all arguments to a Python list"""
 from sys import argv
 
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
 
 def main():
     """adds all arguments to a Python list"""
@@ -15,7 +15,6 @@ def main():
         my_obj = []
     my_obj += argv[1:]
     save_to_json_file(my_obj, "add_item.json")
-
 
 if __name__ == "__main__":
     main()
