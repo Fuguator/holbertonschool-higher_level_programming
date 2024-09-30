dccc86c874c293e6b3cb4231cc762dfb44275a05
Fuguator <abbsvfuad@gmail.com>
2024-09-30 16:06:02 +0400

    Add: 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write_file"""
+
+
+def write_file(filename="", text=""):
+    """write_file"""
+    with open(filename, "a+", encoding='utf-8') as file:
+        return (file.write(text)
