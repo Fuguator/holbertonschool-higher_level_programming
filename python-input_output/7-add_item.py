71408ebc506198b1630f3183473e348739a96b52
Fuguator <abbsvfuad@gmail.com>
2024-09-30 16:48:04 +0400

    Fix_2

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""load_from_json_file"""
+"""adds all arguments to a Python list"""
 from sys import argv
-
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+
 def main():
+    """adds all arguments to a Python list"""
+
     try:
-        obj = load_from_json_file("add_item.json")
+        my_obj = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        obj = []
-        obj += argv[1:]
-        save_to_json_file(obj, "add_item.json")
+        my_obj = []
+    my_obj += argv[1:]
+    save_to_json_file(my_obj, "add_item.json")
+
 
 if __name__ == "__main__":
     main()
