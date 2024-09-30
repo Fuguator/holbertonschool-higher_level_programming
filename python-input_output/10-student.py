155dadab49c192d8e5cd12e063b8396db18ca5fe
Fuguator <abbsvfuad@gmail.com>
2024-09-30 17:25:28 +0400

    Fix_1

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -9,6 +9,6 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if isinstance(attrs, list) and all(type(elem) is str for elem in attrs):
+        if isinstance(attrs, list) and all(type(x) is str for x in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
