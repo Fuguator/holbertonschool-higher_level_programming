f3e0ad09d0e128ece1b9601df4e21671d3b485d6
Fuguator <abbsvfuad@gmail.com>
2024-10-03 11:52:20 +0400

    Fix_1

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,17 +2,19 @@
 """pascal_triangle"""
 
 
-def pascal_triangle(n):
-    """pascal_triangle"""
+def pascal_triangle(n, triangle=None):
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        last_row = triangle[-1]
-        for j in range(len(last_row) - 1):
-            row.append(last_row[j] + last_row[j + 1])
-        row.append(1)
-        triangle.append(row)
-    return triangle
+    if triangle is None:
+        triangle = [[1]]
+
+    if len(triangle) == n:
+        return triangle
+
+    last_row = triangle[-1]
+    row = [1] + [last_row[j] + last_row[j + 1] for j in range(len(last_row) - 1)] + [1]
+
+    triangle.append(row)
+
+    return pascal_triangle(n, triangle)
