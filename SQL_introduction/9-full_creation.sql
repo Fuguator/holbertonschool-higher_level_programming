ec4a551f40c951ea020f3c7a74eeaf79c0e09d49
Fuguator <abbsvfuad@gmail.com>
2024-10-31 18:05:12 +0400

    add

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE second_table (
 );
 
 INSERT INTO second_table
-VALUES (id = 1, name = “John”, score = 10);
-VALUES (id = 2, name = “Alex”, score = 3);
-VALUES (id = 3, name = “Bob”, score = 14);
+VALUES (id = 1, name = “John”, score = 10),
+VALUES (id = 2, name = “Alex”, score = 3),
+VALUES (id = 3, name = “Bob”, score = 14),
 VALUES (id = 4, name = “George”, score = 8);
