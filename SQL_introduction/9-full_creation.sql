5783b68d8c19af0cb73fe86e4e1a1f152e19b344
Fuguator <abbsvfuad@gmail.com>
2024-10-31 18:12:53 +0400

    add

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,11 @@
 --
-CREATE TABLE second_table (
-    id INT
-    name VARCHAR(256)
-    score INT
+CREATE TABLE second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
 );
 
-INSERT INTO second_table (id, name, score)
-VALUES (id = 1, name = “John”, score = 10),
-VALUES (id = 2, name = “Alex”, score = 3),
-VALUES (id = 3, name = “Bob”, score = 14),
-VALUES (id = 4, name = “George”, score = 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
