2436e2c422cdb6de8f49f30ed0c1c0c4d6c25f96
Fuguator <abbsvfuad@gmail.com>
2024-11-02 23:53:12 +0400

    add

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-"""Get all states"""
+""" Lists all states from the database hbtn_0e_0_usa. """
 
-if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb
+import sys
 
-    db = MySQLdb.connect(
-        user=argv[1],
-        password=argv[2],
-        database=argv[3]
-    )
-    cursor = db.cursor()
+import MySQLdb
 
-    cursor.execute('SELECT * FROM states')
-
-    for state in cursor.fetchall():
-        print(state)
-
-    if cursor:
-        cursor.close()
-    if db:
-        db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
