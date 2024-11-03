2d9c5d5c6e03c7d71bc08fc2e37c9c3826ad4e55
Fuguator <abbsvfuad@gmail.com>
2024-11-03 12:29:08 +0400

    add

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+"""salam"""
 
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
