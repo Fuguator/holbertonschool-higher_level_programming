9cd702dd74353dea488af290851fe2c550520618
Fuguator <abbsvfuad@gmail.com>
2024-11-03 18:23:50 +0400

    add

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/python3
+"""Cities by states"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(
+        'SELECT cities.id, cities.name, states.name '
+        'FROM cities '
+        'INNER JOIN states ON cities.state_id = states.id '
+        'ORDER BY cities.id'
+    )
+
+    [print(city) for city in cursor.fetchall()]
+
+    cursor.close()
+    db.close()
