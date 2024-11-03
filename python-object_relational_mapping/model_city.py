75a034c534b59a9fd9cdc297b97bd239f9dc4887
Fuguator <abbsvfuad@gmail.com>
2024-11-03 19:58:16 +0400

    fix

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""City"""
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""City model"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class City(Base):
-    """The City class"""
-    __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+class City(Base):
+    """Cities"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    
