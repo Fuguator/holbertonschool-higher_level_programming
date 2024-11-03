10e4653ad937f315966d152a48d2dea2b4673a9b
Fuguator <abbsvfuad@gmail.com>
2024-11-03 12:32:01 +0400

    add

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""salam"""
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    """salam"""
-    import MySQLdb
-    from sys import argv
-
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
