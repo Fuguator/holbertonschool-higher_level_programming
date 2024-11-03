0d0a6670323822f82f97ef269b5a3f8455e29148
Fuguator <abbsvfuad@gmail.com>
2024-11-03 12:16:49 +0400

    commit

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Module for Selecting states starting with N"""
+"""Filter states"""
+import MySQLdb
+import sys
 
-if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb
-
+if __name__ == "__main__":
     db = MySQLdb.connect(
-        user=argv[1],
-        password=argv[2],
-        database=argv[3]
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
+    
     cursor = db.cursor()
-
-    cursor.execute('SELECT * FROM states ORDER BY id')
-
-    for state in cursor.fetchall():
-        if state[1][0] == 'N':
-            print(state)
-
-    if cursor:
-        cursor.close()
-    if db:
-        db.close()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cursor.fetchall()
+    
+    for state in states:
+        print(state)
+    
+    cursor.close()
+    db.close()
